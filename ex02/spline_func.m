75fbce543ffbdefd838e9ea4f6136843f9a99260
Sergej Keller <sergej.keller@tuhh.de>
2021-10-24 17:23:15 +0000

    Update spline_func.m

--- ex02/spline_func.m
+++ ex02/spline_func.m
@@ -1,12 +1,13 @@
 function [Z] = spline_func(A,X,Y)
 
+# spline function
 B = @(x) max(zeros(size(x)), 1 - abs(x));
 
+# build zero vector
 Z = zeros(size(X));
 
 for i=1:size(A,1)
     for j=1:size(A,2)
-        %Z = Z + A(i, j) .* b_spline(X - i, Y - j);
         Z = Z + A(i, j) .* (B(X - i) * B(Y - j));
     end
 end
