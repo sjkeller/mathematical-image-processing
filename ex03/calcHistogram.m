09e1cb7d9691fd70cf5f62dfa182c65b2ddbed58
Sergej Keller <sergej.keller@tuhh.de>
2021-10-26 14:46:41 +0200

    solution for 3.1 histogram

--- ex03/calcHistogram.m
+++ ex03/calcHistogram.m
@@ -3,7 +3,13 @@ function [H] = calcHistogram(A)
 %
 %   See also histogram, histogram2, histcounts, histcounts2, imhist.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% initialize histogram vector
+H = zeros(256,1);
 
+% iterate through image an sum up grayscale shades
+for c=0:255
+    H(c + 1) = sum(A == c, "all");
+end
 % YOUR CODE HERE
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
