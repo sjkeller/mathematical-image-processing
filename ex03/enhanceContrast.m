bb0d2d6c5e856285d3a9358fd6dcc86a29606f20
Pascal Urban <pascal.urban@tuhh.de>
2021-10-26 17:27:57 +0200

    implemented contrastStretching

--- ex03/enhanceContrast.m
+++ ex03/enhanceContrast.m
@@ -1,11 +1,32 @@
-function [B] = enhanceContrast(A,method)
+function [B] = enhanceContrast(A, method)
 %ENHANCECONTRAST
 %
 %
 %See also adapthisteq, histogram, histeq, imadjust, imcontrast, imhistmatchn.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+B = zeros(size(A));
+
+switch method 
+    case "contrastStretching"        
+        k_min = min(A(:));        
+        k_max = max(A(:));
+        
+        N = 255;
+        
+        for x=1:size(A,1)
+            for y=1:size(A,2)
+                k = A(x, y);
+                B(x, y) = round((k - k_min) / (k_max - k_min) * N);
+            end
+        end
+        
+    case "histEqualization"
+        
+    otherwise
+        disp("error method not found: " + method);
+end
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
