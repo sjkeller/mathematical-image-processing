83c2e2a709990f0d5c3bca89e18889953d25bbdc
Sergej Keller <sergej.keller@tuhh.de>
2021-10-26 15:12:16 +0200

    update

--- ex03/calcHistogram.m
+++ ex03/calcHistogram.m
@@ -3,14 +3,14 @@ function [H] = calcHistogram(A)
 %
 %   See also histogram, histogram2, histcounts, histcounts2, imhist.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% initialize histogram vector
-H = zeros(256,1);
+% get color dimension and initialize histogram vector
+d = size(A,3);
+H = zeros(d,256);
 
 % iterate through image an sum up grayscale shades
 for c=0:255
-    H(c + 1) = sum(A == c, "all");
+    H(:, c + 1) = reshape(sum(A == c, [1,2]),d,1,1);
 end
-% YOUR CODE HERE
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
