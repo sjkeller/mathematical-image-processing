7e45850b656851b02d5659f0a915ef4e8a3ec3bb
Sergej Keller <sergej.keller@tuhh.de>
2021-10-26 17:43:09 +0200

    solution 3.2 hist histEqualization

--- ex03/enhanceContrast.m
+++ ex03/enhanceContrast.m
@@ -5,7 +5,28 @@ function [B] = enhanceContrast(A,method)
 %See also adapthisteq, histogram, histeq, imadjust, imcontrast, imhistmatchn.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+B = zeros(size(A));
+[m,n,d] = size(A);
+N = 255;
+
+switch method
+
+    case "histEqualization"
+
+        H = calcHistogram(A);
+        ohm = numel(A);
+       
+        for t=1:d
+            for i=1:m
+                for j=1:n
+                    B(i,j,t) = round(N/ohm * sum(H(t,1:(A(i,j,t)+1))));
+                end
+            end
+        end
+
+end        
+
 % YOUR CODE HERE
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
