c47eea2fca9a965cf66df27e1607c083771a1e5c
Sergej Keller <sergej.keller@tuhh.de>
2021-11-04 13:51:07 +0100

    solved 4.2

--- ex04/imageDilate.m
+++ ex04/imageDilate.m
@@ -6,31 +6,39 @@ function [V] = imageDilate(U,B)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 V = zeros(size(U));
+mid_x = 0.5 * size(B,1) + 0.5;
+disp(mid_x)
+mid_y = 0.5 * size(B,2) + 0.5;
+disp(mid_y)
 
-mid_x = median(size(B,1));
-mid_y = median(size(B,2));
+m = size(U,1);
+n = size(U,2);
+
+k = unique(U, 'sorted');
 
 % iterate thorugh whole image U
-for x=1:size(U,1)
-    for y=1:size(U,2)
-
-        % check if pixel not white 
-        if U(x,y) < 255
-
-            % iterate around structure element B area inside image U
-            for i=1:size(B,1)
-                for j=1:size(B,2)
-                    
-                    % if indices are not out of bound apply grayscale color
-                    if x-mid_x > 0 && y-mid_y > 0
-                        V(x-mid_x+i, y-mid_y+j) = U(x,y);
+for t=1:size(k)
+    for x=1:m
+        for y=1:n
+    
+            % check if pixel ref 
+            if U(x,y) == k(t)
+    
+                % iterate around structure element B area inside image U
+                for i=1:size(B,1)
+                    for j=1:size(B,2)
+                        if B(i,j) == 1
+                            % if indices are not out of bound apply grayscale color
+                            if x-mid_x+i > 0 && y-mid_y+j > 0 && x-mid_x+i <= m && y-mid_y+j <= n 
+                                V(x-mid_x+i, y-mid_y+j) = U(x,y);
+                            end
+                        end
                     end
-
                 end
+    
             end
-
+    
         end
-
     end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
