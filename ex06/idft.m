0451770cdf1afce948b009bae9b5c433362ab457
Sergej Keller <sergej.keller@tuhh.de>
2021-11-16 15:54:46 +0100

    added idft function

--- ex06/idft.m
+++ ex06/idft.m
@@ -4,7 +4,25 @@ function f = idft(F)
 %   See also fft, ifft, fft2, fftn, fftw.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+% get size of input function
+n = numel(F);
+
+% init omega variable of dft formula
+omega = zeros(1, n + 1);
+
+% preallocate values for omega
+for ii = 1:n+1
+    omega(ii + 1) = (2 * ii * pi) / n;
+end
+
+% sum up inverse fourier coefficients
+for ii=1:4
+    F_tmp = 0;
+    for jj=1:n
+        F_tmp = F_tmp + (F(jj) * exp(1i * omega(jj) * (ii - 1)));
+    end
+    f(ii) = F_tmp / n;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
