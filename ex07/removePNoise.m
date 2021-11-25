97fa32bdb47c206d211bafab9b87742d2ee0f2f4
Pascal Urban <pascal.urban@tuhh.de>
2021-11-25 15:37:18 +0100

    implemented ex7/5.7d

--- ex07/removePNoise.m
+++ ex07/removePNoise.m
@@ -5,7 +5,23 @@
 %See also.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+filtered = applyLp(mandril_pnoise, 63);
+
+figure;
+surf(X,Y,log(1+abs(fftshift(fft2(mandril_pnoise)))),'EdgeColor','none');
+title("Unfiltered frequencies");
+
+M = 512;
+F = fft2(mandril_pnoise);
+[X, Y] = meshgrid(1:M, 1:M);
+
+figure;
+imshow(filtered);
+title("Filtered image");
+
+figure;
+surf(X,Y,log(1+abs(fftshift(fft2(filtered)))),'EdgeColor','none');
+title("Filtered frequencies");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
