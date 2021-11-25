7bdbc856f9779312afc1dfc2eb00f7f15717dcf4
Pascal Urban <pascal.urban@tuhh.de>
2021-11-25 15:37:09 +0100

    implemented ex7/5.7c

--- ex07/applyLp.m
+++ ex07/applyLp.m
@@ -4,7 +4,26 @@ function [B] = applyLp(A,d)
 %See also lowpass, highpass, bandpass, bandstop, filter.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+[M, ~] = size(A);
+u = false(M, M);
+
+lower_bound = int32((M/2 + 1) - d);
+upper_bound = int32((M/2 + 1) + d);
+
+for i = lower_bound:upper_bound
+    for j = lower_bound:upper_bound
+       u(i, j) = true; 
+    end
+end
+
+A_double = im2double(A);
+F = fft2(A_double);
+F_shifted = fftshift(F);
+F_filtered_shifted = F_shifted .*  u;
+F_filtered = fftshift(F_filtered_shifted);
+
+g_double = real(ifft2(F_filtered));
+B = im2uint8(g_double);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
