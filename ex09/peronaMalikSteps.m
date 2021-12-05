1073141ed8fc390b72fb0be8051acb1af12fd17f
Sergej Keller <sergej.keller@tuhh.de>
2021-12-05 21:05:56 +0100

    added files for ex9

---
+++ ex09/peronaMalikSteps.m
@@ -0,0 +1,14 @@
+function [U] = peronaMalikSteps(U_0,kappa,nsteps,dt)
+%PERONAMALIKSTEPS Explicit Euler Iterations for Perona Malik Equation
+%
+%See also.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% YOUR CODE HERE
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
+
+%by NAME
---
+++ ex09/peronaMalikStepsTest.m
@@ -0,0 +1,46 @@
+% Testscript for peronaMalikStep
+
+test_im = zeros(10,'uint8');
+%in = 5*im2uint8(normpdf(1:5,3,3)); %requires statistics package to work
+in = [135  160  170  160  135];
+test_im(2:6,2) = in(1:5)';
+test_im(8,3:7) = in(1:5);
+in_sol = [134  159  169  159  134];
+test_sol =zeros(10,'uint8');
+test_sol(2:6,2) = in_sol(1:5)';
+test_sol(8,3:7) = in_sol(1:5);
+
+test = [];
+no = 1;
+%% datatype test
+try
+    B = peronaMalikSteps(test_im,0.005,1,0.1);
+    test(no) = isa(B,'uint8');
+catch error
+    test(no)=0;
+    disp('An error occured.');
+    disp(error)
+end
+no = no+1;
+
+%% functionality test
+try
+    test_im = peronaMalikSteps(test_im,0.005,300,0.1);
+    
+    test_norm = norm(double(test_im)-double(test_sol));
+    test(no) = (test_norm <1e-8); 
+catch error
+    test(no)=0;
+    disp('An error occured.');
+    disp(error)
+end
+
+%Summary
+disp('Summary:');
+for i=1:length(test)
+    if test(i)
+        fprintf('Test %i: passed\n',i)
+    else
+        fprintf('Test %i: failed\n',i)
+    end
+end
