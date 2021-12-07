b15ab49f648d2891431892828db5b00c1610e979
Pascal Urban <pascal.urban@tuhh.de>
2021-12-07 09:44:18 +0100

    implemented 5.10d

--- ex09/denoisePeppersPMTmpl.m
+++ ex09/denoisePeppersPMTmpl.m
@@ -2,10 +2,18 @@
 %
 %See also.
 
+image = imread("peppers_gray.png");
+noise = imnoise(image,'gaussian', 0, 0.01);
+
+imshow(noise);
+
+denoise = noise;
+
+for i=0:100
+    denoise = peronaMalikSteps(denoise, 0.05, 10, 0.1);
+    imshow(denoise);
+    pause(0.1)
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% YOUR CODE HERE
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
