0cc5baedb98c33c00b00165948a68c8061733a0f
Pascal Urban <pascal.urban@tuhh.de>
2021-12-11 18:37:23 +0100

    adapted ex08 file for ex09

--- ex09/peronaMalikSteps.m
+++ ex09/peronaMalikSteps.m
@@ -5,10 +5,26 @@ function [U] = peronaMalikSteps(U_0,kappa,nsteps,dt)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+% pad image with zeros and convert image
+%dU_0 = zeros(size(U_0));
+dU_0 = im2double(U_0);
+
+g = @(s) 1 ./ (1 + (s.^2)/(kappa^2));
+
+for i=1:nsteps
+    % filter for calculating deltas of image and their neighbours
+    upper = filter2([0 1 1; 0 -1 0; 0 0 0], dU_0);
+    lower = filter2([0 0 0; 0 -1 0; 0 1 0], dU_0);
+    right = filter2([0 0 0; 0 -1 1; 0 0 0], dU_0);
+    left = filter2([0 0 0; 1 -1 0; 0 0 0], dU_0);
+
+    % apply calculated deltas to image pixels and its neighbours (step)
+    dU_0 = dU_0 + dt .* (g(upper) * upper + g(lower) * lower + g(right) * right + g(left) * left);
+end
+
+% reverse image conversion
+U = im2uint8(dU_0);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-end
-
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
