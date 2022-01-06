992f111f78b2dea83201d43c9bd1a2e345c1dec3
Lennart Scherz <Lennart.Scherz@tuhh.de>
2022-01-06 14:06:21 +0100

    added 8.1

--- ex11/applyInpainting.m
+++ ex11/applyInpainting.m
@@ -3,8 +3,31 @@
 % See also.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mandril_img = imread("mandril_defect.png");
+R = [253 260 51 264];
+niter=50;
+d = 29;
 
-% YOUR CODE HERE
+% nose and hair have different frequencies => compromise between both
+% needed.
+
+mandril_new_img = inpaintRect(mandril_img, R, d, niter);
+
+figure(1)
+imshow(mandril_img)
+
+figure(2)
+imshow(mandril_new_img)
+
+% for finding optimal d 
+% for ii = 1:20
+%     d = ii*5;
+%     mandril_new_img = inpaintRect(mandril_img, R, d, niter);
+%     figure(2)
+%     imshow(mandril_new_img)
+%     drawnow
+%     pause(1)
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+%by Sergej Keller, Lennart Scherz, Pascal Urban
--- ex11/inpaintRect.m
+++ ex11/inpaintRect.m
@@ -5,7 +5,21 @@ function B = inpaintRect(A, R, d, niter)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% YOUR CODE HERE
+A_double = im2double(A);
 
+rect_img = A_double(R(1):R(2),R(3):R(4));
+rect_border = A_double(R(1)-1:R(2)+1,R(3)-1:R(4)+1);
+rect_border(2:end-1, 2:end-1) = rect_border(2:end-1, 2:end-1) - rect_img;
+mean_border = sum(rect_border, 'all')/(2*(R(2)-R(1)+2) + 2*(R(4)-R(3)));
+
+A_iter = A_double;
+A_iter(R(1):R(2),R(3):R(4)) = mean_border;
+
+for ii = 1: niter-1
+    A_temp = im2double(applyLp(A_iter,d));
+    A_iter(R(1):R(2),R(3):R(4)) = A_temp(R(1):R(2),R(3):R(4));
+end
+
+B = im2uint8(A_iter);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%by NAME
+% by Sergej Keller, Lennart Scherz, Pascal Urban
